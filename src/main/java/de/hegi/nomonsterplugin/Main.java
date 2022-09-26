009ebf196600333521f4f2e0de95b12dbfd27f62
GamerHegi64 <hegihdonyt@gmail.com>
2022-09-26 22:31:21 +0200

    onEnable message

--- src/main/java/de/hegi/nomonsterplugin/Main.java
+++ src/main/java/de/hegi/nomonsterplugin/Main.java
@@ -2,6 +2,7 @@ package de.hegi.nomonsterplugin;
 
 import de.hegi.nomonsterplugin.listeners.EntitySpawnEventListener;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,7 +10,7 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "No Monster Plugin loaded!");
         PluginManager manager = Bukkit.getPluginManager();
         manager.registerEvents(new EntitySpawnEventListener(), this);
     }
