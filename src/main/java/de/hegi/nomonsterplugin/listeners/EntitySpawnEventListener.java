fa6e797cc0451bff400de777c6f5ef9f7099d157
gamerhegi64 <gamerhegi64@gmail.com>
2022-09-30 23:02:11 +0200

    add missing Monsters

--- src/main/java/de/hegi/nomonsterplugin/listeners/EntitySpawnEventListener.java
+++ src/main/java/de/hegi/nomonsterplugin/listeners/EntitySpawnEventListener.java
@@ -4,35 +4,36 @@ import de.hegi.nomonsterplugin.Main;
 import de.hegi.nomonsterplugin.config.Config;
 import org.bukkit.NamespacedKey;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Monster;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class EntitySpawnEventListener implements Listener {
 
-    @EventHandler
-    public void onEntitySpawn(EntitySpawnEvent event) {
-        Entity entity = event.getEntity();
-        String world_name = event.getLocation().getWorld().getName();
+  @EventHandler
+  public void onEntitySpawn(EntitySpawnEvent event) {
+    Entity entity = event.getEntity();
+    String world_name = event.getLocation().getWorld().getName();
 
-        if (!this.getWorldConfig(world_name)) {
-            return;
-        }
-
-        if (entity instanceof Monster) {
-            event.setCancelled(true);
-        }
+    if (!this.getWorldConfig(world_name)) {
+      return;
     }
 
-    private boolean getWorldConfig(String world_name) {
-        Config config = Main.getInstance().getPluginConfig();
-
-        if (!config.getConfig().contains(world_name + ".stopMonsterSpawning")) {
-            config.getConfig().set(world_name + ".stopMonsterSpawning", true);
-        }
-        return config.getConfig().getBoolean(world_name + ".stopMonsterSpawning");
+    if (entity instanceof Monster || entity instanceof Ghast
+            || entity instanceof Hoglin || entity instanceof Shulker
+            || entity instanceof Slime || entity instanceof EnderDragon) {
+      event.setCancelled(true);
     }
+  }
+
+  private boolean getWorldConfig(String world_name) {
+    Config config = Main.getInstance().getPluginConfig();
+
+    if (!config.getConfig().contains(world_name + ".stopMonsterSpawning")) {
+      config.getConfig().set(world_name + ".stopMonsterSpawning", true);
+    }
+    return config.getConfig().getBoolean(world_name + ".stopMonsterSpawning");
+  }
 
 }
